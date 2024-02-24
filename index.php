9a9475bc969b2873ede2dc3869b3a41b3e564703
shyam duba <shyamduba05@gmail.com>
2024-02-25 01:00:20 +0530

    final layout without using db

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <label>Email :</label>
-        <input type="email" name="email">
-        <label>Genre</label>
-        <input type="text" name="genre">
-        <input type="submit" name="submit">
-    </form>
-</body>
-</html>
-
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
